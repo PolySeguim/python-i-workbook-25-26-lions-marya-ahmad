06dcb1a15b29d03ccd32eb4eca0fd84c9da94998
marya-ahmad <mza206884@gmail.com>
2025-09-18 11:22:44 -0400

    9/19/25

--- moreturtle_examples.py
+++ moreturtle_examples.py
@@ -1,18 +1,38 @@
 import turtle
 
+#screen is a complex data type -
+# meaning that it has attributes and behaviora 
 # Create a turtle object
-geoff = turtle.Turtle()
+screen = turtle.Screen() #this is the instantiation call for the object screen
+screen.title("Turtle Example in Python")
+
+#create a turtle object
+#turtle is a complex data type
+marya = turtle.Turtle() #this is the instantiation of the an object
+marya.color("red")
+marya.shape("turtle")
 
 # Move the turtle forward by 100 units
-geoff.forward(100)
+# --forward100 is a non-fruitful function bexcause 
+# it does not return a value, it simpl preforms an action
+# which is to move the turtle forward
+def forward100():
+    marya.forward(100)
 
 # Turn the turtle to the right by 90 degrees
-geoff.right(90)
+# --right90 is a non-fruitful function because it does
+# not return a value, it simply performs an action
+# which is to turn the turtle right 90 degrees
+def right90():   
+    marya.right(90)
 
-# Repeat the above steps to complete the square
-for _ in range(3):
-geoff.forward(100)
-geoff.right(90)
+#screen is a screen object and it has behaviors
+# like onkey, onkeypress
+screen.onkey(forward100, "Up") 
+screen.onkey(right90, "Right")  
+
+screen.listen() 
+screen.mainloop() 
 
 # Keep the window open until clicked
 turtle.done()
